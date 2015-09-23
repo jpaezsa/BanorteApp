234ec0f8141a74de3d88941c0490fe553f3096a9
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-23 17:30:29 -0500

    :rocket: Add Api Client

---
+++ app/src/main/java/com/example/gerardogtn/banorteapp/service/RetrofitService/RetoBanorteApi/RetoBanorteApiClient.java
@@ -0,0 +1,32 @@
+package com.example.gerardogtn.banorteapp.service.RetrofitService.RetoBanorteApi;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
+/**
+ * Created by gerardogtn on 9/23/15.
+ */
+public class RetoBanorteApiClient {
+    private static RetoBanorteApiService apiService;
+
+    public static RetoBanorteApiService getInstance() {
+        if (apiService == null) {
+            Gson gsonDateBuilder = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                    .create();
+
+            RestAdapter adapter = new RestAdapter.Builder()
+                    .setEndpoint(RetoBanorteApiConstants.URL_BASE)
+                    .setConverter(new GsonConverter(gsonDateBuilder))
+                    .setLogLevel(RestAdapter.LogLevel.BASIC)
+                    .build();
+
+            apiService = adapter.create(RetoBanorteApiService.class);
+        }
+
+        return apiService;
+    }
+}
