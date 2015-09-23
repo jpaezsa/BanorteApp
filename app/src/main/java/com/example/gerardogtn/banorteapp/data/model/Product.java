6b725feab56068b35add97a36bc2419758ed5375
Gerardo Teruel <gerardogtn@gmail.com>
2015-09-23 17:22:49 -0500

    :rocket: Add Product model

---
+++ app/src/main/java/com/example/gerardogtn/banorteapp/data/model/Product.java
@@ -0,0 +1,50 @@
+package com.example.gerardogtn.banorteapp.data.model;
+
+/**
+ * Created by gerardogtn on 9/23/15.
+ */
+public class Product {
+    private int id;
+    private String type;
+    private String name;
+    private String description;
+
+    public Product(int id, String type, String name, String description) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
